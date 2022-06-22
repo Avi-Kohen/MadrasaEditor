4538a29dd81bf107204b6e4df043f1689e8f2de1
avi-kohen <avicohen1021@gmail.com>
2022-06-22 17:13:59 +0300

    added edit window Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout
 import sys
 
 index = 1
@@ -18,24 +18,53 @@ class Sentence:
 
 
 # GUI
-class Window(QWidget):
+
+class EditorWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Madrasa Editor')
+        self.setWindowTitle('New Conversation')
+
+        self.title_box = QLineEdit(self)
+        self.title_box.setPlaceholderText('Title')
+        self.description_box(self)
+        self.description_box.setPlaceholderText('Description')
+
+
+        grid = QGridLayout()
+        grid.addWidget(self.title_box)
+        self.setLayout(grid)
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Madrasa Bot Editor')
         self.setWindowIcon(QIcon("madrasa.png"))
         self.setGeometry(500, 300, 550, 300)
+
+        self.welcome_label = QLabel("Welcome to Madrasa Bot Editor!", self)
+        self.welcome_label.move(190, 50)
 
         self.choose()
 
     def choose(self):
-        btn_1 = QPushButton("New Conversation", self)
-        btn_2 = QPushButton("Existing Conversation", self)
+        new_btn = QPushButton("New Conversation", self)
+        existing_btn = QPushButton("Existing Conversation", self)
 
-        btn_1.setGeometry(50, 150, 200, 100)
-        btn_2.setGeometry(300, 150, 200, 100)
+        new_btn.setGeometry(50, 150, 200, 100)
+        existing_btn.setGeometry(300, 150, 200, 100)
+
+        new_btn.clicked.connect(self.clicked_new)
+        # existing_btn.clicked.connect(self.clicked_existing)
+
+    def clicked_new(self):
+        editor.show()
+        self.hide()
+
+    # def clicked_existing(self):
 
 
 app = QApplication(sys.argv)
-window = Window()
+editor = EditorWindow()
+window = MainWindow()
 window.show()
 app.exec()
