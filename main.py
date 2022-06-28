849007d445682e15c102161e235e01e0a89224f1
avi-kohen <avicohen1021@gmail.com>
2022-06-28 04:25:47 +0300

    finished first sentence

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os.path
 from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
 from pydub import AudioSegment
 from pydub.playback import play
-from PyQt6.QtCore import QUrl
+from PyQt6.QtCore import QUrl, Qt
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout
 import sys
@@ -15,19 +15,24 @@ content = dict()
 flow = dict()
 sentences = dict()
 
+
 # class Tree:
 #     def __init__(self):
 #         self.
 
 # GUI
 class SentenceWindow(QWidget):
-    def __init__(self):
+    def __init__(self, parent=None):
         super().__init__()
+        self.parent = parent
         self.recFile = None
         self.setWindowTitle('Sentence adder')
         self.setGeometry(500, 300, 350, 300)
         flow[str(index)] = []
         self.label = QLabel("Enter sentence ")
+        self.parent_sound = "test_" + str(parent) + ".wav"
+        self.parent_voice = QPushButton('Answer to this')
+        self.parent_voice.hide()
         self.arabic = QLineEdit(self)
         self.arabic.setPlaceholderText('Arabic')
         self.hebrew = QLineEdit(self)
@@ -38,6 +43,7 @@ class SentenceWindow(QWidget):
         self.record_btn.setText('Record')
         self.voice = QPushButton(self)
         self.voice.setIcon(QIcon("play-button.png"))
+        self.voice.setEnabled(False)
         self.keywords = QLineEdit(self)
         self.keywords.setPlaceholderText("Keywords (separated by comma and a space)")
         self.done = QPushButton(self)
@@ -49,19 +55,25 @@ class SentenceWindow(QWidget):
         self.enter.clicked.connect(self.create_sentence)
         self.add_response = QPushButton(self)
         self.add_response.setText("Add Response")
-        self.advance = QPushButton(self)
-
+        self.add_response.setEnabled(False)
+        # self.advance = QPushButton(self)
+        # self.advance.setText("Advance")
 
         grid = QGridLayout()
-        grid.addWidget(self.label)
-        grid.addWidget(self.arabic)
-        grid.addWidget(self.hebrew)
-        grid.addWidget(self.transcription)
-        grid.addWidget(self.record_btn)
-        grid.addWidget(self.voice)
-        grid.addWidget(self.keywords)
-        grid.addWidget(self.done, 7, 1, 2, 1)
-        grid.addWidget(self.enter,7,0,2,1)
+        grid.addWidget(self.label, 0, 0)
+        grid.addWidget(self.parent_voice, 0, 1)
+        grid.addWidget(self.arabic, 1, 0, 1, 1)
+        grid.addWidget(self.hebrew, 2, 0, 1, 1)
+        grid.addWidget(self.transcription, 3, 0, 1, 1)
+        grid.addWidget(self.record_btn, 4, 0, 1, 1)
+        grid.addWidget(self.voice, 5, 0, 1, 1)
+        grid.addWidget(self.keywords, 6, 0, 1, 1)
+        grid.addWidget(self.done, 7, 1, 1, 1)
+        grid.addWidget(self.enter, 7, 0, 1, 1)
+        grid.addWidget(self.add_response, 8, 0, 1, 1)
+        # grid.addWidget(self.advance,8,1,1,1)
+
+        self.add_response.clicked.connect(self.continues_talking)
 
         self.setLayout(grid)
         self.keepRecording = True
@@ -70,24 +82,37 @@ class SentenceWindow(QWidget):
         self.record_btn.clicked.connect(self.on_click)
         self.voice.clicked.connect(self.play_sound)
 
+    def continues_talking(self):
+        global index, window
+        window = SentenceWindow(parent=index)
+        index += 1
+        window.parent_voice.show()
+        window.show()
+
     def create_sentence(self):
-        sentences[str(index)] = {"id_" : str(index), "arabic":self.arabic.text(), "arabicWithoutDiacritics":self.arabic.text()
-                                 ,"hebrew":self.hebrew.text(),"transcription": self.transcription.text(),
-                                 "voiceRecPath":"test_" + str(index) + ".wav", "keywords":self.keywords.text().split(", ")}
+        keyword = self.keywords.text().split(", ")
+        keyword.remove("")
+        sentences[str(index)] = {"id_": str(index), "arabic": self.arabic.text(),
+                                 "arabicWithoutDiacritics": self.arabic.text()
+            , "hebrew": self.hebrew.text(), "transcription": self.transcription.text(),
+                                 "voiceRecPath": "test_" + str(index) + ".wav",
+                                 "keywords": keyword}
         self.done.setEnabled(True)
         self.enter.setEnabled(False)
         self.record_btn.setEnabled(False)
+        self.add_response.setEnabled(True)
 
     def finish(self):
-        content["flow"] = flow
-        content["sentences"] = sentences
-        json_object = json.dumps(content, indent=4, ensure_ascii=False)
+        if self.parent is None:
+            content["flow"] = flow
+            content["sentences"] = sentences
+            json_object = json.dumps(content, indent=4, ensure_ascii=False)
 
-        # Writing to sample.json
-        with open("conversation.json", "w") as outfile:
-            outfile.write(json_object)
-
-        sys.exit()
+            with open("conversation.json", "w") as outfile:
+                outfile.write(json_object)
+            sys.exit()
+        else:
+            self.close()
 
     def play_sound(self):
         self.full_file_path = os.path.join(os.getcwd(), 'test.wav')
@@ -101,10 +126,12 @@ class SentenceWindow(QWidget):
             self.recFile.start_recording()
             self.record_btn.setText("Stop")
             self.button_state = 'stop'
+            self.voice.setEnabled(False)
         else:
             self.recFile.stop_recording()
             self.record_btn.setText("Record")
             self.button_state = 'record'
+            self.voice.setEnabled(True)
             # self.clear_texts()
 
 
@@ -147,7 +174,6 @@ class EditorWindow(QWidget):
         self.next_button.clicked.connect(self.next_window)
 
     def next_window(self):
-        self.hide()
         adder.show()
         content["id"] = "mock-script"
         content["title"] = self.title_box.text()
@@ -157,7 +183,7 @@ class EditorWindow(QWidget):
         content["category"] = self.category_box.text()
         content["createdBy"] = self.createdBy_box.text()
         content["start"] = str(index)
-
+        self.hide()
         # json_object = json.dumps(content, indent=4, ensure_ascii=False)
         #
         # # Writing to sample.json
@@ -189,7 +215,7 @@ class MainWindow(QWidget):
 
     def clicked_new(self):
         editor.show()
-        self.hide()
+        self.close()
 
     # def clicked_existing(self):
 
